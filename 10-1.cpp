566b6dc298d6154ec2ff0c2506e03e2248e41961
qiaoyunhao1234 <qyh13260703097@163.com>
2026-01-23 16:59:08 +0800

    10-1

---
+++ 10-1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+struct TreeNode{
+    char data;
+    TreeNode* leftChild;
+    TreeNode* rightChild;
+    TreeNode(char c):data(c),leftChild(NULL),rightChild(NULL){}
+};
+
+TreeNode* Build(int& position,string str){
+    char c=str[position++];
+    if(c=='#'){
+        return NULL;
+    }
+    TreeNode* root=new TreeNode(c);
+    root->leftChild=Build(position,str);
+    root->rightChild=Build(position,str);
+    return root;
+}
+
+void InOrder(TreeNode* root){
+    if(root==NULL){
+        return;
+    }
+    InOrder(root->leftChild);
+    printf("%c",root->data);
+    InOrder(root->rightChild);
+    return;
+}
+int main(){
+    string str;
+    while(cin>>str){
+        int position=0;
+        TreeNode* root=Build(position,str);
+        InOrder(root);
+        printf("\n");
+    }
+    return 0;
+}
