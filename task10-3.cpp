c584d46e4c828b61b2fcd10628bee36e49b86b71
qiaoyunhao1234 <qyh13260703097@163.com>
2026-02-02 16:34:11 +0800

    similar to 10-6

---
+++ task10-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+
+
+
+int main(){
+    int n;
+    while(scanf("%d", &n) != EOF){
+        priority_queue<int, vector<int>, greater<int>> myPriorityQueue;
+        while(n--){
+            int x;
+            scanf("%d", &x);
+            myPriorityQueue.push(x);    
+        }
+        int answer = 0;
+        while(myPriorityQueue.size() > 1){
+            int a = myPriorityQueue.top();
+            myPriorityQueue.pop();
+            int b = myPriorityQueue.top();
+            myPriorityQueue.pop();
+            answer += a + b;
+            myPriorityQueue.push(a + b);
+        }
+        printf("%d\n", answer);
+    }
+}
