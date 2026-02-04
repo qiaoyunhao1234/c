d8c25a56b3025c688001ed7f46e09eb6d8fa3cda
qiaoyunhao1234 <qyh13260703097@163.com>
2026-02-04 20:32:41 +0800

    汉诺塔

---
+++ 8递归与分治/8-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+long long Function(int n){
+    if(n == 1){
+        return 2;             //递归终止条件
+    }else{
+        return 3*Function(n-1)+2;
+    }
+}
+
+int main(){
+    int n;
+    while(scanf("%d",&n)!=EOF){
+        printf("%lld\n",Function(n));
+    }
+}
