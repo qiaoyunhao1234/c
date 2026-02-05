bc67d84ce1610e795eec6ac0c7999d4ce8d10fcd
qiaoyunhao1234 <qyh13260703097@163.com>
2026-02-05 11:34:31 +0800

    格式化输出

--- 6数学问题/6-1.cpp
+++ 6数学问题/6-1.cpp
@@ -7,13 +7,25 @@ using namespace std;
 int main(){
     int a;
     while(scanf("%d",&a)!=EOF){
+        int n = a;
+        int bits = 0, temp = n;
+        while (temp) {
+            bits++;
+            temp >>= 1;
+        }//bits表示二进制位数，7（111）的bits为3
+        if (bits == 0) bits = 1; // n为0时特殊处理
+        
+        int width = ((bits + 3) / 4) * 4; // 补齐到4的倍数
+
+
         vector<int> binary;
-        while(a!=0){
-            binary.push_back(a%2);
-            a/=2;
+        temp = n;
+        for(int i = 0; i < width; ++i) {
+            binary.push_back(temp % 2);
+            temp /= 2;
         }
-        for(int i=binary.size()-1;i>=0;--i){
-            printf("%d",binary[i]);
+        for(int i = width - 1; i >= 0; --i) {
+            printf("%d", binary[i]);
         }
         printf("\n");
     }
