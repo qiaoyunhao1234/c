536c3bb21098de5baf011e805d51702269fc1577
qiaoyunhao1234 <qyh13260703097@163.com>
2026-02-05 17:00:39 +0800

    使用sqrt

--- 6数学问题/6-7.cpp
+++ 6数学问题/6-7.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<cstdio>
+#include<cmath>
 using namespace std;
 
-int main(){
+/* int main(){
     int n;
     while(scanf("%d",&n)!=EOF){
         bool flag=false;
@@ -13,6 +14,29 @@ int main(){
             }
         }
         printf("%s\n",flag?"No":"Yes");
+    }
+    return 0;
+} */
+
+
+bool judge(int n){
+    if(n<2) return false;
+    int bound=sqrt(n);
+    for(int i=2;i<=bound;++i){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    while (scanf("%d",&n)!=EOF)
+    {
+        if(judge(n)){
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
     }
     return 0;
 }
