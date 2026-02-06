5e75f04050fd64295236476dca93f4ff530e9a6f
qiaoyunhao1234 <qyh13260703097@163.com>
2026-02-06 12:14:45 +0800

    快速幂 A^B

---
+++ 6数学问题/6-10.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int FastExponentiation(int a,int b,int mod){
+    int answer=1;
+    while(b!=0){
+        if(b%2==1){
+            answer*=a;
+            answer%=mod;
+        }
+        b/=2;
+        a*=a;
+        a%=mod;
+    }
+    return answer;
+
+}
+
+
+
+
+int main(){
+    int a,b;
+    while(scanf("%d %d",&a,&b)!=EOF){
+        if(a==0&&b==0){
+            break;
+        }
+        printf("%d\n", FastExponentiation(a, b, 1000));
+    }
+    return 0;
+}
