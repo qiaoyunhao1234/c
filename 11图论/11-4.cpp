22e6dcd459db1e2a4a552439092c5bd0a894cee8
qiaoyunhao1234 <qyh13260703097@163.com>
2026-02-08 15:04:45 +0800

    Kruskal最小生成树

---
+++ 11图论/11-4.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+const int MAXN=100;
+struct Edge{
+    int from;
+    int to;
+    int length;
+    bool operator<(const Edge& e) const{
+        return length<e.length;
+    }
+};
+Edge edge[MAXN*MAXN];
+int father[MAXN];
+int height[MAXN];
+
+void Initial(int n){
+    for(int i=0; i<n; i++){
+        father[i]=i;
+        height[i]=0;
+    }
+}
+int Find(int x){
+    if(x!=father[x]){
+        father[x]=Find(father[x]);
+    }
+    return father[x];
+}
+
+void Union(int x, int y){
+    x=Find(x);
+    y=Find(y);
+    if(x!=y){
+        if(height[x]<height[y]){
+            father[x]=y;
+        }else if(height[y]<height[x]){
+            father[y]=x;
+        }else{
+            father[y]=x;
+            height[x]++;
+        }
+    }
+    return;
+}
+int Kruskal(int n, int edgeNumber){
+    Initial(n);
+    sort(edge, edge+edgeNumber);
+    int sum=0;
+    for(int i=0;i<edgeNumber;i++){
+        Edge current=edge[i];
+        if(Find(current.from)!=Find(current.to)){    //Find不等说明不再一个集体，可以选进来
+            Union(current.from,current.to);          //放进来成为一个集体
+            sum+=current.length;                     //加入当前边的长度
+        }
+    }
+    return sum;
+
+}
+
+
+
+int main(){
+    int n;
+    while(scanf("%d", &n)!=EOF){
+        if(n==0) break;
+        int edgeNumber = n*(n-1)/2;
+        for(int i=0; i<edgeNumber; i++){
+            scanf("%d%d%d",&edge[i].from,&edge[i].to,&edge[i].length);
+        }
+        int answer=Kruskal(n, edgeNumber);
+        printf("%d\n", answer);
+    }    return 0;
+}
