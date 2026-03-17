d795d0dd3a9903844caff461d10139d8b9db9965
qiaoyunhao1234 <qyh13260703097@163.com>
2026-03-17 10:48:08 +0800

    11.9

--- 11图论/11-9.cpp
+++ 11图论/11-9.cpp
@@ -8,23 +8,27 @@ using namespace std;
 
 const int MAXN=501;
 vector<int> graph[MAXN];
-int inDegree[MAXN];
+int inDegree[MAXN];    //入度
 
 vector<int> TopologicalSort(int n){
-    vector<int>topology;
-    priority_queue<int,vector<int>,greater<int>>node;
+    vector<int>topology;   //用于输出最终排名
+    priority_queue<int,vector<int>,greater<int>> node;     //小根堆，每次访问或取出为最小值
     for(int i=1;i<=n;i++){
         if(inDegree[i]==0){
             node.push(i);
         }
     }
+
     while(!node.empty()){
         int u=node.top();
-        node.pop();
-        topology.push_back(u);
+        node.pop();     //弹出最强的
+
+        topology.push_back(u);   //最强的放进去
+
         for(int i=0;i<graph[u].size();i++){
             int v=graph[u][i];
             inDegree[v]--;
+
             if(inDegree[v]==0){
                 node.push(v);
             }
@@ -40,15 +44,17 @@ int main(){
         while(m--){
             int from,to;
             scanf("%d%d",&from,&to);
-            graph[from].push_back(to);
+            graph[from].push_back(to);     //邻接表
             inDegree[to]++;
         }
-        vector<int>answer=TopologicalSort(n);
+        vector<int> answer=TopologicalSort(n);
+
+        //输出最终排名
         for(int i =0;i<answer.size();i++){
             if(i==0){
                 printf("%d",answer[i]);
             }else{
-                printf("%d",answer[i]);
+                printf(" %d",answer[i]);
             }
         }
         printf("\n");
